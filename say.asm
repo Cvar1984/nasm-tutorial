8b5faab570bf464268156e70a8a90ae2cbf7b8ff
Cvar1984 <cvar1984@cvar1984.net>
2025-05-20 17:31:12 +0700

    input with stack

--- say.asm
+++ say.asm
@@ -1,19 +1,51 @@
-extern printf
+; register parameter position
+; 1s     rdi
+; 2nd    rsi
+; 3rd    rdx
+; 4th    rcx
+; 5th    r8
+; 6th    r9
+; Extra arguments (after 6) go on the stack.
+extern  printf
 
 section .rodata
-    msg db "Hello World", 0xa,0 ; newline, null terminator
+    msg    db "Input something: ",0 ; null terminator
+    msg2   db "You entered: ", 0
     format db "%s",0
 
 section .text
     global main
+    extern printf ; int printf(const char *restrict format, ...);
+    extern scanf  ; int scanf(const char *restrict format, ...);
 
 main:
-    push rbp; Create a stack-frame, re-aligning the stack to 16-byte alignment before calls
-    mov rdi, format; %s
-    mov rsi, msg; Hello World\n
+    push rbp
+    mov  rbp, rsp
+
+    ; using mov
+    mov  rdi, format
+    mov  rsi, msg
+    xor  eax, eax    ; vardiac functions expect AL=0
     call printf
 
-    pop rbp
-    mov rdi, rax
-    mov rax, 60
-    syscall
+    ; using stack
+    lea  rdi, [rel format]
+    sub  rsp, 8            ; reserve 8 bytes for input
+    lea  rsi, [rsp]        ; use address of empty stack memory (&input)
+    mov  rax, rsi
+    push rax
+    xor  eax, eax
+    call scanf
+
+    mov  rdi, format
+    mov  rsi, msg2
+    xor  eax, eax    ; vardiac functions expect AL=0
+    call printf
+
+    lea  rdi, [rel format]
+    pop  rsi               ; second arg from the stack
+    xor  eax, eax
+    call printf
+
+    leave
+    ret
